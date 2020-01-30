fd45e41e0edae9b618ecf88730ad376f550ce94f
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-30 05:05:52 +0300

    live

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Book;
 use App\Models\User;
+use App\Models\Writeup;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,5 +20,25 @@ class DatabaseTest extends TestCase
         $user = factory(User::class)->create(['role_id' => 35]);
         $this->actingAs($user);
         $this->get('/')->assertSee('Cytonn Libraries');
+    }
+    public function test_adding_a_book_to_the_database()
+    {
+        Event::fake();
+        $this->withoutExceptionHandling();
+        $user = factory(User::class)->create(['role_id' => 35]);
+        $this->post('books',[
+            'name' => 'This is Maggie',
+            'descritption' => 'Gal is writtng tests' ,
+            'status_id' => 6,
+        ]);
+        $this->assertCount(1,Book::all());
+//        $this->assertDatabaseHas('writeups', array $data);
+    }
+    public function test_getting_blogs_from_database()
+    {
+        $user = factory(User::class)->create();
+        factory(Book::class)->create(['status_id' => 6]);
+        $response = $this->get('books');
+        $response->assertStatus(201);
     }
 }
